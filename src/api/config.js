677a118ddd4ae3bb7d043c89784204f1e7d655eb
artiely <artiely1220@gmail.com>
2017-08-18 16:00:04 +0800

    修改配置

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,15 @@
+
+var config = require('../../config')
+var isProduction = process.env.NODE_ENV === 'production'
+if (isProduction) {
+  console.log('%c 如果你看到这条 log , 说明当前是生产环境', 'font-size:14px;color:#f00;background:#000')
+} else {
+  console.log('%c 如果你看到这条 log , 说明当前是开发环境', 'font-size:14px;color:#f00;background:#000')
+}
+// 服务器地址
+export const SERVER_BASE_URL = isProduction ? config.build.baseServerUrl : config.dev.baseServerUrl
+
+
 // global.SERVER_BASE_URL = 'http://c.fwone.com/PT'; //线上地址
 global.SERVER_BASE_URL = 'http://172.16.7.248:8087/fwone-central';
 
