fc2f5ee4399fd32441958215bafd635d74eca1c3
artiely <artiely1220@gmail.com>
2017-08-22 09:01:59 +0800

    修改配置

--- src/api/config.js
+++ src/api/config.js
@@ -7,10 +7,10 @@ if (isProduction) {
   console.log('%c 如果你看到这条 log , 说明当前是开发环境', 'font-size:14px;color:#f00;background:#000')
 }
 // 服务器地址
-export const SERVER_BASE_URL = isProduction ? config.build.baseServerUrl : config.dev.baseServerUrl
+// export const SERVER_BASE_URL = isProduction ? config.build.baseServerUrl : config.dev.baseServerUrl
 
 
 // global.SERVER_BASE_URL = 'http://c.fwone.com/PT'; //线上地址
-global.SERVER_BASE_URL = 'http://172.16.7.248:8087/fwone-central';
+global.SERVER_BASE_URL = isProduction ? config.build.baseServerUrl : config.dev.baseServerUrl
 
 global.ERR_OK = 0
